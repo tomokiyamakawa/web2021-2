f61df1f25fe6f6a889b5548ecbb4a1c3480620f2
tomokiyamakawa <zm19328@josai.ac.jp>
2022-01-22 08:25:43 +0000

    0122

---
+++ ex6-13.js
@@ -0,0 +1,1 @@
+function img() {document.getElementById('logo5').src ="./image/josailogo.png";}
---
+++ ex6-14.js
@@ -0,0 +1,2 @@
+function imgchange4() {document.getElementById('logo4').src='./image/josailogo.png';}
+function bthrees() {document.getElementById('logo4').src='./image/keiei_small.jpg';}
